50ccac22c478cd8f11b9016211f826d853e78eff
Joseph Munoz <munozj_1999@yahoo.com>
2020-05-20 14:34:51 -0700

    fixes for queries

--- app.js
+++ app.js
@@ -11,26 +11,11 @@ app.get('/', (req, res) => {
     res.send('Welcome to the student grade app')
 })
 
-module.exports = app;
+app.post('/', (req, res) => {
+    res.send('Welcome to the student grade app')
+})
 
+module.exports = app;
 
 //this is done in bin/www
 //app.listen(app.get('port'), () => console.log('listening on port ${app.get('port')}'))
-
-
-// var express = require('express');
-// var path = require('path');
-// var app = express();
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-// var app = express();
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// module.exports = app;
--- routes/students.js
+++ routes/students.js
@@ -5,25 +5,28 @@ const fs = require('fs')
 
 const students = JSON.parse(fs.readFileSync('students.json'))
 
-//gets all students
-router.get('/', (req, res) => res.send(students))
-
 //gets student by id
-router.get('/id/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     const selectedStudent = students.find(student => student.id === Number(req.params.id))
     res.send(selectedStudent)
 })
 
-//gets students by name
-router.get('/name/:name', (req, res) => {
-    const selectedStudent = students.filter(student => student.name.includes(req.params.name))
-    res.send(selectedStudent)
-});
-
-//gets students by grade
-router.get('/grade/:grade', (req, res) => {
-    const selectedStudent = students.filter(student => student.grade.includes(req.params.grade))
-    res.send(selectedStudent)
+//gets students by grade, name or all
+router.get('/', (req, res) => {
+    if(req.query.grade)
+    {
+        const selectedStudent = students.filter(student => student.grade.includes(req.query.grade))
+        res.send(selectedStudent)
+    }
+    else if(req.query.name)
+    {
+        const selectedStudent = students.filter(student => student.name.includes(req.query.name))
+        res.send(selectedStudent)
+    }
+    else
+    {
+        res.send(students)
+    }
 });
 
 //posts student's grade
